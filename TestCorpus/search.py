3a337d8f2facc578b20d8f51d0055430fbde1d1e
Elmira <cclikespizza@gmail.com>
2015-11-19 02:08:09 +0300

    Initial commit

---
+++ TestCorpus/search.py
@@ -0,0 +1,171 @@
+# coding=utf-8
+__author__ = 'elmira'
+
+import re
+import codecs
+from annotator.models import Document, Sentence, Annotation, Token, Morphology
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from db_utils import Database
+
+# todo make this into a neat one-line js-function
+jquery = """jQuery(function ($) {$('#***').annotator().annotator('addPlugin', 'Tags').annotator('addPlugin', 'Corr').annotator('addPlugin', 'Correction', 'Enter correct variant...').annotator('addPlugin', 'ReadOnlyAnnotations').annotator('addPlugin', 'Store', {prefix: '/learner_corpus/document-annotations',annotationData: {'document': ***},loadFromSearch: {'document': ***}});});"""
+reg = re.compile(',| ')
+
+def get_subcorpus(query):
+    req = 'SELECT id FROM `annotator_document` WHERE 1 '
+    if u'checked' in query:
+        req += 'AND checked=True '
+    if u'annotated' in query:
+        req += 'AND annotated=True '
+    gender = query.get(u'gender').encode('utf-8')
+    if gender != u'any':
+        req += 'AND gender="'+ gender +'" '
+    date1 = query.get(u'date1')
+    if date1 != u'':
+        req += 'AND date1>='+ date1 +' '
+    date2 = query.get(u'date2')
+    if date2 != u'':
+        req += 'AND date2<='+ date2 +' '
+    genre = query.getlist(u'genre[]')
+    if genre != []:
+        req += make_small_query(genre, 'genre')
+    major = query.getlist(u'major[]')
+    if major != []:
+        req += make_small_query(major, 'major')
+    course = query.getlist(u'course[]')
+    if course != []:
+        req += make_small_query(course, 'course')
+    db = Database()
+    docs = [str(i[0]) for i in db.execute(req)]
+    subsum = db.execute('SELECT SUM(sentences), SUM(words) FROM `annotator_document` WHERE id IN (' +req + ')')
+    flag = False if req == 'SELECT id FROM `annotator_document` WHERE 1 ' else True
+    return docs, subsum[0][0], subsum[0][1], flag
+
+
+def make_small_query(arr, title):
+    one = []
+    s = ''
+    for l in arr:
+        one.append(title + '="'+ l.encode('utf-8') +'"')
+    if len(one) == 1:
+        s += 'AND '+ one[0] + ';'
+    else:
+        s += 'AND (' + ' OR '.join(one) + ')'
+    return s
+
+
+def bold(word, sent):
+    s = re.sub('('+word+')', '<b>\\1</b>', sent)
+    # s = sent
+    # r = re.findall('<span .+?</span>',  sent)
+    # for i in range(len(r)):
+    #     if word in r[i]:
+    #         s = re.sub('('+r[i]+')', '<b>\\1</b>', s)
+    return s
+
+
+def pages(sent_list, page, num):
+    paginator = Paginator(sent_list, num)
+    try:
+        sents = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        sents = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        sents = paginator.page(paginator.num_pages)
+    return sents
+
+
+def exact_search(word, docs, flag):
+    db = Database()
+    # db.cur.execute('SELECT tok.sent_id, tok.doc_id, sent.text FROM `annotator_token` tok, `annotator_sentence` sent WHERE tok.token="дом" and tok.sent_id=sent.id;')
+    req1 = 'SELECT COUNT(DISTINCT doc_id) FROM `annotator_token` WHERE token="'+word + '" '
+    if flag:
+        req1 += 'AND doc_id IN ('+','.join(docs) + ');'
+    docs_len = int(db.execute(req1)[0][0])
+    req2 = 'SELECT DISTINCT sent_id FROM `annotator_token` WHERE token="'+ word +'" '
+    if flag:
+        req2 += 'AND doc_id IN ('+','.join(docs) + ');'
+    tokens = db.execute(req2)
+    # tokens = Token.objects.filter(token__exact=word)
+    jq = []
+    sent_list = []
+    for row in tokens:
+        try:
+            sent_list.append(Sentence.objects.get(pk=row[0]))
+        except:
+            pass
+    for sent in tokens:
+        # sent.temp = bold(word, sent.tagged)
+        # sent.save()
+        jq.append(jquery.replace('***', str(sent[0])))
+    return jq, sent_list, word, docs_len
+
+def lex_search(query, docs, flag):
+    # print query
+    words = query.getlist(u'wordform[]')
+    lexis = query.getlist(u'lex[]')
+    grams = query.getlist(u'grammar[]')
+    errs = query.getlist(u'errors[]')
+    # print query.getlist(u'major'), query.getlist(u'genre')
+    # froms = query.getlist(u'from[]')
+    # tos = query.getlist(u'to[]')
+    # <QueryDict: u'wordform[]' u'date1':  u'grammar[]': [u'S', u'A'], [u''], u'format': [u'full'], u'errors[]': u'from[]' u'major':  u'sort_by': [u'wordform'], u'additional[]': u'gender' u'genre'  u'per_page': [u'10'], u'expand': [u'+-1'], u'to[]'}>
+    sent_list = {}
+    jq = []
+    # for wn in xrange(len(words)):
+    wn = 0
+    word = words[wn].lower().encode('utf-8')
+    lex = lexis[wn].encode('utf-8')
+    gram = grams[wn].encode('utf-8')
+    err = errs[wn].encode('utf-8')
+    rows = collect_data([word, lex, gram, err, docs, flag])
+    sent_list = [Sentence.objects.get(pk=row[0]) for row in rows]
+    for sent in rows:
+        jq.append(jquery.replace('***', str(sent[0])))
+    word=' '.join(query.getlist(u'wordform[]'))
+    return jq, sent_list, word, len(set([s.doc_id for s in sent_list]))
+
+
+def collect_data(arr):
+    word, lex, gram, err, docs, flag = arr
+    if [word, lex, gram] == ["", "", ""] and err != '':
+        req = 'SELECT document_id FROM annotator_annotation WHERE 1 '
+        errs = [i for i in reg.split(err.lower()) if i != '']
+        for er in errs:
+            req += 'AND tag LIKE "%' + er + '%" '
+    else:
+        if err != '':
+            req = '''SELECT DISTINCT sent_id FROM  annotator_token
+        LEFT JOIN annotator_morphology
+        ON annotator_token.id = annotator_morphology.token_id
+        LEFT JOIN annotator_annotation
+        ON annotator_token.sent_id = annotator_annotation.document_id
+        WHERE 1 '''
+            errs = [i for i in reg.split(err.lower()) if i != '']
+            for er in errs:
+                req += 'AND tag LIKE "%' + er + '%" '
+            req += 'AND num>= annotator_annotation.start AND num <= annotator_annotation.end '
+        else:
+            req = '''SELECT DISTINCT sent_id FROM  annotator_token
+        LEFT JOIN annotator_morphology
+        ON annotator_token.id = annotator_morphology.token_id
+        WHERE 1 '''
+        if word != '':
+            req += 'AND lem="'+word+'" '
+        if lex != '':
+            req += 'AND lex LIKE "%' + lex + '%" '
+        if gram != '':
+            grams = [i for i in reg.split(gram) if i != '']
+            for gr in grams:
+                req += 'AND gram LIKE "%' + gr + '%" '
+    if flag:
+        req += 'AND doc_id IN ('+','.join(docs)+');'
+    # f = codecs.open('s.txt', 'w')
+    # f.write(req)
+    # f.close()
+    db = Database()
+    rows = db.execute(req)
+    return rows
---
+++ TestCorpus/views.py
@@ -0,0 +1,135 @@
+# -*- coding=utf-8 -*-
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, redirect, render
+#from models import Doc, Sentence, Error, Analysis, Token
+from annotator.models import Document, Sentence, Annotation, Token, Morphology
+from django.views.generic.base import View
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from forms import QueryForm
+from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseServerError, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden
+from django.views.generic import View
+from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.conf import settings
+from django.template import *
+from search import *
+from collections import Counter
+from django.utils.translation import ugettext_lazy as _
+
+
+import re
+rePage = re.compile(u'&page=\\d+', flags=re.U)
+
+from django.forms.formsets import formset_factory
+
+
+class Struct:
+    def __init__(self, **values):
+        vars(self).update(values)
+
+
+class Index(View):
+
+    def get(self, request, page):
+        doc_list = Document.objects.all()
+        # эта функция просто достает нужный шаблон и показывает его
+        if page == '':
+            return render_to_response(u'start.html', {'docs': doc_list}, context_instance=RequestContext(request))
+        page = 'simple/' + page + '.html'
+        return render_to_response(page, {'docs': doc_list}, context_instance=RequestContext(request))
+
+
+class PopUp(View):
+
+    def get(self, request, page):
+        page = 'search/' + page + '.html'
+        return render_to_response(page, context_instance=RequestContext(request))
+
+class Search(Index):
+    # тут все для поиска
+
+    # todo write search
+    def get(self, request, page):  # page does nothing here, just ignore it
+        if len(request.GET) < 1:
+            QueryFormset = formset_factory(QueryForm, extra=2)
+            d = {}
+            d['genres'] = set([i.genre for i in Document.objects.all() if i.genre not in [u"", None, ' ']])
+            d['course'] = set([i.course for i in Document.objects.all() if i.course not in [u"", None, ' ']])
+            d['major'] = set([i.major for i in Document.objects.all() if i.major not in [u"", None, ' ']])
+            d['domain'] = set([i.domain for i in Document.objects.all() if i.domain not in [u"", None, ' ']])
+            return render_to_response('search.html',
+                                      {'form': QueryFormset, 'd': d},
+                                      context_instance=RequestContext(request))
+        else:
+            query = request.GET
+            subcorpus, subcorpus_sents, subcorpus_words, flag = get_subcorpus(query)
+            # print subcorpus.count()
+            # subcorpus_sents = [sent.id for doc in subcorpus[0] for sent in doc.sentence_set.all()]
+            count_data = {'total_docs': Document.objects.count(),
+                          'total_sents': Sentence.objects.count(),
+                          'total_tokens': Token.objects.count(),
+                          'subcorpus_docs': len(subcorpus),
+                          'subcorpus_sents': subcorpus_sents,
+                          'subcorpus_words': subcorpus_words}
+            per_page = int(query.get(u'per_page'))
+            if query["exact_word"] != '':
+                jq, sent_list, word, res_docs = exact_search(request.GET["exact_word"].lower().encode('utf-8'), subcorpus, flag)
+
+            else:
+                # QueryFormset = formset_factory(QueryForm)
+                # formset = QueryFormset(request.GET, request.FILES)
+                # if formset.is_valid():
+                # todo rewrite this part of search
+                jq, sent_list, word, res_docs = lex_search(query, subcorpus, flag)
+
+            page = request.GET.get('page')
+            sents = pages(sent_list, page, per_page)
+            # if page:
+            #     start = page - 10 if page - 10 > 0 else 1
+            #     end = page + 10 if page + 10 <= sents.paginator.num_pages else sents.paginator.num_pages
+            # else:
+            #     start = 1
+            #     end = 11
+            # sents.paginator.page_range = range(start, end)
+            full_path = rePage.sub('', request.get_full_path())
+            return render_to_response('result.html',
+                                      {'query': word, 'result': sents,
+                                       'numbers': count_data,
+                                       'total': len(sent_list), 'total_docs': res_docs,
+                                       'path':full_path, 'j':jq},
+                                      context_instance=RequestContext(request))
+
+
+class Statistics(Index):
+
+    def get(self, request, page):
+        docs = Document.objects.count()
+        doc_ann = Document.objects.filter(annotated=True).count()
+        doc_ann_percent = int(100*float(doc_ann)/docs)
+        doc_check = Document.objects.filter(checked=True).count()
+        doc_check_percent = int(100*float(doc_check)/docs)
+        sents = Sentence.objects.count()
+        words = Token.objects.count()
+        annotations = Annotation.objects.count()
+        gender = dict(Counter([i.gender for i in Document.objects.all()]))
+        genres = dict(Counter([i.genre for i in Document.objects.all()]))
+        course = dict(Counter([i.course for i in Document.objects.all()]))
+        major = dict(Counter([i.major for i in Document.objects.all()]))
+        domain = dict(Counter([i.domain for i in Document.objects.all()]))
+
+        return render_to_response('stats.html', {'docs':docs,
+                                                 'progress': [doc_ann, doc_ann_percent,
+                                                              doc_check, doc_check_percent],
+                                                 'sents':sents,
+                                                 'words':words,
+                                                 'annot':annotations,
+                                                 'gender':gender,
+                                                 'genres':genres,
+                                                 'course': course,
+                                                 'major': major,
+                                                 'domain': domain},
+                                  context_instance=RequestContext(request))
+# todo write login \ registration (if needed??)
